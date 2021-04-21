fbe09b8a1bf5b831e327a2516336f0e432d1b435
JeetKaneriya <jeet352002@gmail.com>
2021-04-21 10:46:00 +0530

    commit

--- FemFluid/urls.py
+++ FemFluid/urls.py
@@ -15,19 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from MyApp.sitemaps import StaticViewSitemap
-from django.contrib.sitemaps.views import sitemap
-
-sitemaps = {
-    'static': StaticViewSitemap
-}
-
-handler404 = 'MyApp.views.handler404'
-handler500 = 'MyApp.views.handler500'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MyApp.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('robots.txt', include('robots.urls')),
 ]
