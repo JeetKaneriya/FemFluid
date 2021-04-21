273b5fba37bf633f4e4b564fa827b99bb2006669
JeetKaneriya <jeet352002@gmail.com>
2021-04-21 11:03:26 +0530

    commit

--- FemFluid/urls.py
+++ FemFluid/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = 'MyApp.views.handler404'
+handler500 = 'MyApp.views.handler500'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MyApp.urls')),
