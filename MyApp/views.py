348070aef7ad95213fac954b1f967992db50bdfc
JeetKaneriya <jeet352002@gmail.com>
2021-04-21 10:54:17 +0530

    commit

--- MyApp/views.py
+++ MyApp/views.py
@@ -76,7 +76,52 @@ def ip_save(request):
 
 
 def home(request):
-    return render(request, 'index.html')
+    status = {"run": 2}
+    request.session['login'] = False
+
+    if not request.session.get('ipCheck', False):
+        ip_save(request)
+
+    if request.method == 'POST':
+
+        # Reading data for the user complaint
+        subject = "Response from the client through contact us"
+        from_email = request.POST['email']
+        message = request.POST['message']
+        name = request.POST['name']
+        contact_num = request.POST['contact-num']
+        designation = request.POST['designation']
+        formatted_message = '<center><div style="width:600px"><img src="https://test-femfluid.herokuapp.com/static/assets/images/Email-Header.png" style="margin-bottom: 50px; width:100%;"><p style="color:#0b5394; font-size:14px; text-align: left; margin-left:30px; ">From <strong>' + name + '</strong>,<br><br>' + message + '<br><br>Contact no. : <strong>' + contact_num + '</strong><br>Designation : <strong>' + designation + '</strong></p><p style="text-align: left"><strong><strong><br></strong></strong></p><div style="background-color:#2A58A3; padding:1px 0"><p style="text-align: center;color:#fff; font-size: 12px; font-weight: 300; letter-spacing: 0.5px;"><strong><strong>Copyright © 2021 <a href="https://fem-fluid.herokuapp.com/" style="color:#fff; text-decoration:none;">FEM FLUID</a> | Designed by <a href="https://www.linkedin.com/in/coding-comrades-654b3120a/" style="color:#fff; text-decoration:none;" target="blank">Cod\'rades</a></strong></strong></p></div></div></center>'
+        to_email_list = ['jeet352002@gmail.com', 'hitpatel1107@gmail.com', 'tandelneelkanth@gmail.com']
+
+        # Computing data for the auto-generated mail
+        response_subject = "Thank you for getting in touch!"
+        response_mail = '<center><div style="width:600px"><img src="https://test-femfluid.herokuapp.com/static/assets/images/Email-Header.png" style="margin-bottom: 50px; width:100%;"><p style="color:#0b5394; font-size:14px; text-align: left; margin-left:30px; ">Dear <strong>' + name + '</strong>,<br><br>We appreciate you contacting us.<br>One of our colleagues will get back in touch with you soon!<br><br>Have a great day!<br><br><strong>From Cod\'rades Team.</strong></p><p style="text-align: left"><strong><strong><br></strong></strong></p><div style="background-color:#2A58A3; padding:1px 0"><p style="text-align: center;color:#fff; font-size: 12px; font-weight: 300; letter-spacing: 0.5px;"><strong><strong>Copyright © 2021 <a href="http://fem-fluid.herokuapp.com/" style="color:#fff; text-decoration:none;">FEM FLUID</a> | Designed by <a href="https://www.linkedin.com/in/coding-comrades-654b3120a/" style="color:#fff; text-decoration:none;" target="blank">Cod\'rades</a></strong></strong></p></div></div></center>'
+
+        # User complaint api details ( mailgun )
+        complaint_host = 'smtp-relay.sendinblue.com'  # 'smtp.mailgun.org'
+        complaint_port = 587
+        complaint_username = 'jeet352002@gmail.com'  # 'postmaster@sandboxfbc7c0a359694fc9818c8520713fc6a5.mailgun.org'
+        complaint_password = 'Mq67Ns0fzXGptFrD'  # '37d32e369ef3fe65622f1077aaf9e7bb-29561299-f6bb2df3'
+        complaint_use_tls = True
+        complaint_connection = get_connection(host=complaint_host, port=complaint_port, username=complaint_username, password=complaint_password, use_tls=complaint_use_tls)
+
+        # Auto generated mail api details ( sendgrid )
+        response_host = 'smtp-relay.sendinblue.com'  # 'smtp.sendgrid.net'
+        response_port = 587
+        response_username = 'jeet352002@gmail.com'  # 'apikey'
+        response_password = 'Mq67Ns0fzXGptFrD'  # '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'
+        response_use_tls = True
+        response_connection = get_connection(host=response_host, port=response_port, username=response_username, password=response_password, use_tls=response_use_tls)
+
+        try:
+            send_mail(response_subject, response_mail, 'jeet352002@gmail.com', [from_email], fail_silently=False, connection=response_connection)
+            send_mail(subject, formatted_message, from_email, to_email_list, fail_silently=False, connection=complaint_connection)
+            status['run'] = 1
+        except:
+            status['run'] = 0
+
+    return render(request, 'index.html', status)
 
 
 def sd_series(request):
